b11d5b6df1958793eb74a05af32ffa1457360768
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-03-23 07:23:34 +0000

    updated

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+let output = document.getElementById('output');
+let arr = [1,2,3,4];
+
+let promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let evenArray = arr.filter((item) => item % 2 == 0);
+        resolve(evenArray);
+    }, 1000);
+});
+
+promise1.then((data) => {
+    output.textContent = `${data}`;
+}).catch((error) => {
+    console.log(error);
+});
+
+let promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        let multEvenArray = arr.filter((item) => item % 2 == 0)
+                           .map((item) => item * 2);
+        resolve(multEvenArray);
+    }, 2000);
+});
+
+promise2.then((data) => {
+    output.textContent = `${data}`;
+}).catch((error) => {
+    console.log(error);
+});
